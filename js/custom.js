5daffefef905c2d53da8449b6c98658f66943f41
00FreeDragon00 <2662851@163.com>
2016-11-29 19:24:08 +0900

    commit

--- js/custom.js
+++ js/custom.js
@@ -124,17 +124,3 @@ $(document).ready(function() {
 
    $(".fancybox").fancybox();
 });
-
-wow = new WOW({
-    animateClass: 'animated',
-    offset: 100
-});
-wow.init();
-document.getElementById('').onclick = function() {
-    var section = document.createElement('section');
-    section.className = 'wow fadeInDown';
-    section.className = 'wow shake';
-    section.className = 'wow zoomIn';
-    section.className = 'wow lightSpeedIn';
-    this.parentNode.insertBefore(section, this);
-};
